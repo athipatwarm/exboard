d531dff02a5255a12787decec7672de931f53141
Athipat <athipatpingwong@gmail.com>
2024-12-23 14:03:21 +0700

    ..

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,11 +6,11 @@ const {auth,adminAuth} = require('../middleware/auth.js')
 router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.post('/logout', auth, userController.logoutUser);
-router.post('/users', adminAuth, userController.createUser);
+router.post('/users', userController.createUser);
 router.get('/users', userController.getAllUsers);
 router.patch('/users/me', userController.updateProfile);
-router.get('/users/:id', adminAuth, userController.getUserById);
+router.get('/users/:id', userController.getUserById);
 router.patch('/users/:id', userController.updateUser);
-router.delete('/users/:id', adminAuth, userController.deleteUser);
+router.delete('/users/:id', userController.deleteUser);
 
 module.exports = router;
