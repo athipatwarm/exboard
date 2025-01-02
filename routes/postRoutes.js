ac33c6072e196cae97e16ad708873fbed82debd7
Athipat <athipatpingwong@gmail.com>
2025-01-02 11:04:34 +0700

    add post button

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const postController = require('../controllers/postController');
 const { auth } = require('../middleware/auth');
 
-router.post('/posts', authMiddleware, postController.createPost);
+router.post('/posts', auth, postController.createPost);
 
 module.exports = router;
